0ba45a42c63bca4676628127d4b7136088887093
ChuckDK <chuck.lagoni@gmail.com>
2013-11-19 22:06:20 +0100

    Fiflet med eclipse

--- u1/src/P13.java
+++ u1/src/P13.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 public class P13 {
 
   public static void main(String[] args) {
-	String aa = "";
-	String result = "";
-	System.out.print("Indtast et nummer: ");
 
-    aa = (new Scanner(System.in).next());
+    String result;
+    System.out.print("Indtast et nummer: ");
+
+    String aa = new Scanner(System.in).next();
 
     switch (aa.length()) {
       case 4:
-        result = aa.substring(0,1) + "," + aa.substring(1);
+        result = aa.substring(0, 1) + "," + aa.substring(1);
         break;
       case 5:
-        result = aa.substring(0,2) + "," + aa.substring(2);
+        result = aa.substring(0, 2) + "," + aa.substring(2);
         break;
       case 6:
-        result = aa.substring(0,3) + "," + aa.substring(3);
+        result = aa.substring(0, 3) + "," + aa.substring(3);
         break;
       default:
         result = "Ugyldigt input";
         break;
-	  }
+    }
 
-	  System.out.println(result);
+    System.out.println(result);
   }
 
 }

a5ab5dd32b309a0b1dc4f188fa0ae6c74939b1dd
erikallin <erik.allin@gmail.com>
2013-11-25 17:30:31 +0100

    Tilføjet mappe til genaflevering af u1 og lavet P13 samt rapport til

---
+++ u1genafl/src/P13.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Klassen tilføjer kommaer til et vilkårligt tal og returnerer tallet. 
+ */
+public class P13 {
+
+  public static void main(String[] args) {
+
+    String result;
+ 
+    System.out.print("Indtast et nummer mellem 1000 og 999999: ");
+
+    String input = new Scanner(System.in).next();
+    int last =  input.length();
+    result = input.substring(0,(last-3)) + "," + input.substring((last-3),last);
+    
+    
+   // BONUS: Hvis tallet ikke er i intervallet printes "Ugyldigt input".
+   if (input.length() > 3 && input.length() < 7) 
+   {
+    System.out.println(result); 
+   }
+   else 
+   { 
+     System.out.println("Ugyldigt input");
+   }
+  }
+}

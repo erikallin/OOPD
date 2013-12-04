0e2f3355d8e08a1345730687671ad7c513fc22e5
peterspliid <pspliid@gmail.com>
2013-12-04 14:21:06 +0100

    Bumbum

--- u3/src/ComputerPlayer.java
+++ u3/src/ComputerPlayer.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class ComputerPlayer {
 
-  ComputerPlayer() {
+  public ComputerPlayer(boolean difficulty) {
+    Game n = new Game();
 
+    difficulty ? smartMove() : dumbMove();
+  }
+  public void smartMove(){
+
+  }
+  public void dumbMove(){
+  int antal = Random.nextint(n.getHeap()/2)+1;
+  n.remove(antal);
+  
   }
 }

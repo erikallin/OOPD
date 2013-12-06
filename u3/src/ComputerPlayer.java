a1b810feeff375a68b7206c357b805fa85c56352
Peter Spliid <pspliid@gmail.com>
2013-12-06 03:56:35 +0100

    ComputerPlayer "done"
    + classes. Deal with it

--- u3/src/ComputerPlayer.java
+++ u3/src/ComputerPlayer.java
@@ -13,7 +13,19 @@ public class ComputerPlayer {
      dumbMove();
   }
   public void smartMove(){
-
+    if (n.getHeap() <= 3)
+      n.remove (n.getHeap() - 1);
+    else if (n.getHeap() > 3 && n.getHeap() <= 7)
+      n.remove (n.getHeap() - 3);
+    else if (n.getHeap() > 7 && n.getHeap() <= 15)
+      n.remove (n.getHeap() - 7);
+    else if (n.getHeap() > 15 && n.getHeap() <= 31)
+      n.remove (n.getHeap() - 15);
+    else if (n.getHeap() > 31 && n.getHeap() <= 63)
+      n.remove (n.getHeap() - 31);
+    else
+      n.remove (n.getHeap() - 63);
+      
   }
   public int dumbMove(){
   return (new Random().nextInt(n.getHeap()/2)+1);

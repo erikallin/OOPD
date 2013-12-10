82407141f5b9b0dd8064ac993e5876a6154d2c87
ChuckDK <Chuck>
2013-12-10 15:23:10 +0100

    u4 we will kill you

---
+++ u4/src/Appointment.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Appointment {
+  private int year, month, day;
+  private String appointment;
+  
+  public Appointment() {
+  
+  }
+  
+  public void setYear(int y) {
+    this.year = y;
+  }
+  
+  public void setMonth(int m) {
+    this.month = m;
+  }
+  
+  public void setDay(int d) {
+    this.day = d;
+  }
+
+
+
+}
---
+++ u4/src/Monthly.java
@@ -0,0 +1,3 @@
+public class Monthly implements Appointment {
+
+}

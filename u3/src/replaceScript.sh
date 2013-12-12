7d300f97fbfefd1f1f4b6bb80594896f700a6a50
erikallin <erik.allin@gmail.com>
2013-12-12 22:47:45 +0100

    Ryddet helt op fra Eclipse

--- u3/src/replaceScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Author: Chuck
-# Replaces all tabs(\t) with two tabs and removes trailing whitespaces
-# Don't be retarded when you use this
-
-sed -i 's/\t/  /g;s/[[:space:]]*$//' $1
-echo "All tabs and trailing whitespaces in \"$1\"\
- has been replaced with two spaces."
